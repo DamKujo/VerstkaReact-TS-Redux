4215d9b11aa24d7244bafc8141d19d74d17a8a5f
Dam Kujo <shurupov.96@list.ru>
2024-08-23 16:22:41 +0300

    fourth commit

--- src/components/Reviews/reviews.tsx
+++ src/components/Reviews/reviews.tsx
@@ -5,7 +5,7 @@ export default function Reviews(){
     return (
         <div className='content-blue'>
             <div className='wrapper'>
-                <div className='content-title mrgtop'>
+                <div className='content-title'>
                     <h2>Отзывы</h2>
                 </div>
                 <Slider/>
